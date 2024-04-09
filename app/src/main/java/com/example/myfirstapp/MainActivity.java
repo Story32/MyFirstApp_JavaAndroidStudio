1e60217478915ebc381ab7739b3c3ebd81246a94
Awes <awes1994@hotmail.com>
2024-04-09 13:04:39 +0200

    Insert name to press button and get it in the list

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.btn_mybutton);
+        EditText editText = findViewById(R.id.inp_name);
+        TextView textView = findViewById(R.id.txt_box);
+
+//        private void buttonClickingAndLollygagging()
+
+        btn.setOnClickListener(view -> {
+          textView.setText("Dit navn er: " + editText.getText());
+        });
+
+
+    }
+}
